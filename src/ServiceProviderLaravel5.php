d0f66c2b8772aa08c26e997fda686778eddf35ed
Logan Schmidt <d4rkd0s@gmail.com>
2016-09-07 09:44:35 -0500

    Added publish tag to maknz/slack for Laravel 5

--- src/ServiceProviderLaravel5.php
+++ src/ServiceProviderLaravel5.php
@@ -14,7 +14,7 @@ class ServiceProviderLaravel5 extends \Illuminate\Support\ServiceProvider
      */
     public function boot()
     {
-        $this->publishes([__DIR__.'/config/config.php' => config_path('slack.php')]);
+        $this->publishes([__DIR__.'/config/config.php' => config_path('slack.php')], 'maknz/slack');
     }
 
     /**
