61f92d74cafe7ce4c3085d38919b8b9e76b76bde
Muhamad Yulianto <mail@mjniuz.com>
2017-09-29 14:00:09 +0700

    update lv 5.5

--- src/ServiceProviderLaravel5.php
+++ src/ServiceProviderLaravel5.php
@@ -26,7 +26,7 @@ class ServiceProviderLaravel5 extends \Illuminate\Support\ServiceProvider
     {
         $this->mergeConfigFrom(__DIR__.'/config/config.php', 'slack');
 
-        $this->app['maknz.slack'] = $this->app->singleton('maknz.slack', ($app) {
+        $this->app->singleton('maknz.slack', ($app) {
             return new Client(
                 $app['config']->get('slack.endpoint'),
                 [
