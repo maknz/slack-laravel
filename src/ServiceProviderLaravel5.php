a07ab0c27c9c6cc1c324aa2f0991ec8a2dfab898
Rohan Sakhale <rs@saiashirwad.com>
2017-12-15 10:35:16 +0530

    Singleton usage update

--- src/ServiceProviderLaravel5.php
+++ src/ServiceProviderLaravel5.php
@@ -26,17 +26,17 @@ class ServiceProviderLaravel5 extends \Illuminate\Support\ServiceProvider
     {
         $this->mergeConfigFrom(__DIR__ . '/config/config.php', 'slack');
 
-        $this->app['maknz.slack'] = $this->app->singleton('maknz.slack', function ($app) {
+        $this->app->singleton('maknz.slack', function ($app) {
             return new Client(
                 $app['config']->get('slack.endpoint'),
                 [
-                    'channel' => $app['config']->get('slack.channel'),
-                    'username' => $app['config']->get('slack.username'),
-                    'icon' => $app['config']->get('slack.icon'),
-                    'link_names' => $app['config']->get('slack.link_names'),
-                    'unfurl_links' => $app['config']->get('slack.unfurl_links'),
-                    'unfurl_media' => $app['config']->get('slack.unfurl_media'),
-                    'allow_markdown' => $app['config']->get('slack.allow_markdown'),
+                    'channel'                 => $app['config']->get('slack.channel'),
+                    'username'                => $app['config']->get('slack.username'),
+                    'icon'                    => $app['config']->get('slack.icon'),
+                    'link_names'              => $app['config']->get('slack.link_names'),
+                    'unfurl_links'            => $app['config']->get('slack.unfurl_links'),
+                    'unfurl_media'            => $app['config']->get('slack.unfurl_media'),
+                    'allow_markdown'          => $app['config']->get('slack.allow_markdown'),
                     'markdown_in_attachments' => $app['config']->get('slack.markdown_in_attachments'),
                 ],
                 new Guzzle
