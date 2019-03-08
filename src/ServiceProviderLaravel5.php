4ccd384f2e54da5471b3ae5fdca0fe4f067cd71b
Brandon Saunders <bsaunders@jnrcorp.COM>
2019-03-08 07:36:07 -0800

    Reference Laravel's singleton instead of share method.

--- src/ServiceProviderLaravel5.php
+++ src/ServiceProviderLaravel5.php
@@ -25,8 +25,8 @@ class ServiceProviderLaravel5 extends \Illuminate\Support\ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/config/config.php', 'slack');
-
-        $this->app['maknz.slack'] = $this->app->share(function ($app) {
+    
+        $this->app->singleton('maknz.slack', function ($app) {
             return new Client(
                 $app['config']->get('slack.endpoint'),
                 [
